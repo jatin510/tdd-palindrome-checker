8253a1d92dc8d824e323fddd907d873ddc72e803
jatin510 <jatin6972@gmail.com>
2022-10-01 22:34:18 +0530

    added the logic to check if the string is palindrome or not

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 export class PalindromeChecker {
   isAPalindrome(str: string) {
-    if (str === 'mom') {
-      return true;
-    } else {
-      return false;
-    }
+    const reverse = str.split('').reverse().join('');
+    return reverse === str;
   }
 }
